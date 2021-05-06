241f0acd060cf77d4794b02c948c7f416d9f75bf
ashutosh-874 <64722998+ashutosh-874@users.noreply.github.com>
2021-05-06 13:52:41 +0530

    Add: db connection

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import {selectMovies} from '../features/movie/movieSlice'
+import { useSelector } from 'react-redux'
 
 function Movies() {
+
+    const movies = useSelector(selectMovies)
+
+    
+
     return (
         <Container>
             <h4>
                 Recommended for You
-                <Content>
-                    <Wrap>
-                        <img src="/images/viewers-disney.png" alt="" /> 
-                    </Wrap>
-                    <Wrap>
-                        <img src="/images/viewers-disney.png" alt="" /> 
-                    </Wrap>
-                    <Wrap>
-                        <img src="/images/viewers-disney.png" alt="" /> 
-                    </Wrap>
-                    <Wrap>
-                        <img src="/images/viewers-disney.png" alt="" /> 
-                    </Wrap>
-                </Content>
             </h4>
+            <Content>
+                {
+                    movies
+                    &&
+                    movies.map(movie => (
+                        <Wrap key={movie.id}>
+                            <img src={movie.cardImg} alt = '' />
+                        </Wrap>
+                    ))
+                }
+            </Content>
         </Container>
     )
 }
